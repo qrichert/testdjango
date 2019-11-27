b72543874fabb0b5fc063f1773e390587703c025
Quentin Richert <quentin@quentinrichert.com>
2019-11-27 18:20:30 +0100

    Hello world

--- testdjango/urls.py
+++ testdjango/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import app.views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', app.views.home),
+    path('home', app.views.home)
 ]
